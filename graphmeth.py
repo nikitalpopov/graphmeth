533c74f122b00379eeef1562110ffc9b215032e3
tapochek97 <tapochek97@ya.ru>
2016-12-21 17:25:49 +0300

    commit

--- graphmeth.py
+++ graphmeth.py
@@ -1,19 +1,25 @@
 import numpy as np
 import init
 import scipy.optimize
+import matplotlib.pyplot as plt
 from solution import *
 
 print('Коэффициенты целевой функции:')
 c = init.c
-c = np.matrix([[1], [2]])
+c = np.matrix([[1], [3]])
 print(c)
 print('Коэффициенты ограничений:')
 a = init.a
-a = np.matrix([[5, 3], [3, 4], [1, 0]])
+a = np.matrix([[3, 2], [1, 2], [-1, 1], [0, 1], [-1, 0], [0, -1]])
 print(a)
 print('Правая часть ограничений:')
 b = init.b
-b = np.matrix([[1], [2], [3]])
+b = np.matrix([[12], [6], [1], [2], [0], [0]])
 print(b)
 
-x = sysSolution(a, b)
+vect = sysSolution(a, b)
+x = vect[:][0]
+y = vect[:][1]
+
+plt.plot(x, y, 'ro')
+plt.show()
--- solution.py
+++ solution.py
@@ -4,37 +4,38 @@ import init
 import newton
 
 
-def sysSolution(a, b): # решение 'A * x = b'
+def sysSolution(a, b):  # решение 'A * x = b'
     counter = 0
-    vect = []
+    vect = np.zeros((init.k, init.n*init.n))
     tempA = np.zeros((2, 2))
     tempB = np.zeros((2, 1))
     tempX = np.zeros((2, 1))
-    print('tempX: ',tempX)
+    isUsable = True
+
     for i in range(init.n):
         for k in range(init.n):
             tempA = np.zeros((2, 2))
             tempB = np.zeros((2, 1))
             tempX = np.zeros((2, 1))
             if not i == k:
-                # print(i, ' != ', k)
+                print(i, ' != ', k)
                 for l in range(init.k):
-                    # print(l)
                     tempA[0][:] = a[i][:]
                     tempA[1][:] = a[k][:]
-                    tempB[i][0] = b[i][0]
-                    tempB[k][0] = b[k][0]
-                # print(tempA)
-                # print(tempX)
-                # print(tempB)
+                    tempB[0][0] = b[i][0]
+                    tempB[1][0] = b[k][0]
 
-                while la.norm(tempB) >= 0.000001:
-                    c = la.solve(tempA, tempB)
-                    tempX -= c
+                if not la.det(tempA) == 0:
+                    tempX = la.solve(tempA, tempB)
 
-                for m in range(init.k):
-                    vect[counter][m] = tempX[0][m]
-            print(counter)
+                for z in range(tempX.size):
+                    if tempX[z][0] < 0:
+                        isUsable = False
+
+                if isUsable:
+                    for m in range(init.k):
+                        vect[m][counter] = tempX[m][0]
             counter += 1
-    print('Vect:', vect)
+    # print('Vect:', vect)
+    return vect
 
