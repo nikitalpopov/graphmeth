a30fc8251e3240091a8437749dcfb728d0a340c0
tapochek97 <tapochek97@ya.ru>
2016-12-21 15:19:21 +0300

    add 'init.py', 'solution.py'

--- graphmeth.py
+++ graphmeth.py
@@ -1,2 +1,9 @@
+import numpy as np
+import init
 import scipy.optimize
+import newton
 
+print('Коэффициенты целевой функции:')
+print(init.c)
+print('Коэффициенты ограничений:')
+print(init.a)
---
+++ init.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+k = 3 # Кол-во переменных
+n = 6 # Кол-во ограничений
+c = np.zeros((1, k))
+a = np.zeros((n, k))
